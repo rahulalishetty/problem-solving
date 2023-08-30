db747d7fc34b93fc4184ae1ea2c01844f77408e5
Rahul Alishetty <ralishetty@spreadsheet.com>
2023-08-30 21:11:00 +0530

    Tree problems

--- Trees/SearchBST.js
+++ Trees/SearchBST.js
@@ -1,12 +1,8 @@
 // Given a Binary Search Tree A. Check whether there exists a node with value B in the BST.
 
 function searchBST(root, target) {
-    if(!root) return 0;
-    if(root.data === target) return 1;
-
-    let left, right;
-    if(root.data > target) left = searchBST(root.left);
-    if(root.data < target) right = searchBST(root.right);
-
-    return left || right;
+    while(root !== null && root.data !== target) {
+        root = root.data > target ? root.left : root.right;
+    }
+    return root === null ? 0 : 1;
 }
